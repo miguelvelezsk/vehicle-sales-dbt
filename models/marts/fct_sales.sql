a185e93ad3e28251d79395c7decfa162968c3aa7
miguelvelezsk <miguelvelezaguirre10@gmail.com>
2026-05-05 08:42:59 -0500

    feat: generate fct_sales mart and add corresponding documentation

--- models/marts/fct_sales.sql
+++ models/marts/fct_sales.sql
@@ -9,7 +9,14 @@ category_metrics AS (
 final AS (
     SELECT
         s.*,
-        c.percentage_share_by_category
+        c.percentage_share_by_category,
+
+        date_trunc('month', s.order_date) AS order_month,
+        date_trunc('quarter', s.order_date) AS order_quarter,
+        date_trunc('year', s.order_date) AS order_year,
+        extract('month' from s.order_date) AS order_month_number,
+        dayname(s.order_date) AS order_day_of_week
+
     FROM staging s
     LEFT JOIN category_metrics c
         ON s.order_id = c.order_id
